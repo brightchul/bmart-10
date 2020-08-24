9a811dec9dade605da535e18a44fa65bd3161472
changicho <changicho29@gmail.com>
2020-08-24 16:34:09 +0900

    feat: 불필요한 props 삭제
    props로 flicking 객체를 할당해 조작하던 방식 수정함
    
    이는 대표상품의 메뉴 탭 내부에서 모든것을 해결하도록 수정했기 때문

--- frontend/src/components/home/Recommend/List.tsx
+++ frontend/src/components/home/Recommend/List.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Flicking from "@egjs/react-flicking";
 import styled from "styled-components";
 
 import Content from "./Content";
@@ -22,9 +21,6 @@ const Text = styled.div`
 type Props = {
   innerHeight?: number;
   contentHeight?: number;
-  store: {
-    flicking: Flicking | undefined;
-  };
   menus: Array<string>;
 };
 
@@ -34,7 +30,6 @@ export default function List(props: Props): JSX.Element {
       {props.menus.map((menu, index) => (
         <Content key={`${menu}.${index}`} innerHeight={HEIGHT}></Content>
       ))}
-
       <Text>
         <p>더이상 대표 상품이 없어요</p>
       </Text>
