e18b4c296f8ca6dae0123d9af2d1abba7f08fbe4
이슬기 <tmfrl002@gmail.com>
2020-08-25 12:17:50 +0900

    HOTFIX!: 프론트엔드 context 폴더 변경
    - import된 파일이름이 틀린 부분 수정(context/popupContext.tsx -> PopupContext.tsx)
    - context폴더 안에 있는 파일들을 contexts 폴더로 이동

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Router from "./router";
 import { PopUp } from "./components/common";
-import { PopUpContext, CartContext } from "./context";
+import { PopUpContext, CartContext } from "./contexts";
 
 function App(): JSX.Element {
   return (
