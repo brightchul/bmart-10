c42d1b2f6fccb7af254f46e35cff99ca49adbc00
changicho <changicho29@gmail.com>
2020-08-26 21:13:28 +0900

    feat: API 적용
    대표상품을 가져오는 API 적용

--- backend/src/apis/index.ts
+++ backend/src/apis/index.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import goodsAPI from "./goodsAPI";
 import categoryAPI from "./categoryAPI";
 import userAPI from "./userAPI";
+import recommendAPI from "./recommendAPI";
 
 const router = Router();
 
 router.use("/goods", goodsAPI);
 router.use("/category", categoryAPI);
 router.use("/user", userAPI);
+router.use("/recommend", recommendAPI);
 
 export default router;
