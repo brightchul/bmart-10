b00b96616549b4ffe78c42f99898b65bb4b29c36
changicho <changicho29@gmail.com>
2020-08-26 14:45:14 +0900

    feat: 검색 기록 닫기 버튼 추가
    애니메이션을 위해 닫기 버튼을 누르고 0.3s 이후 닫히도록 구성

--- frontend/src/components/search/History/index.tsx
+++ frontend/src/components/search/History/index.tsx
@@ -2,26 +2,55 @@ import React from "react";
 import styled from "styled-components";
 
 import { getHistory } from "../../../utils/localstorage";
-import { useSearchState } from "../../../contexts/SearchContext";
+import {
+  useSearchState,
+  useSearchDispatch,
+} from "../../../contexts/SearchContext";
 
 import HistoryText from "./HistoryText";
+import CloseButton from "./CloseButton";
 
 const Wrapper = styled.div`
-  margin-top: 80px;
+  padding: 10px;
+  border-radius: 10px;
+  margin: 10px;
+
+  background-color: #eee;
+`;
+
+const List = styled.div``;
+
+const Footer = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
 `;
 
 export default function SearchBar(): JSX.Element {
   const state = useSearchState();
+  const dispatch = useSearchDispatch();
 
   return (
-    <Wrapper>
-      {state.showHistory
-        ? getHistory()
-            .reverse()
-            .map((history, index) => (
-              <HistoryText key={`history.${index}`}>{history}</HistoryText>
-            ))
-        : null}
-    </Wrapper>
+    <>
+      {state.showHistory ? (
+        <Wrapper>
+          <List>
+            {getHistory()
+              .reverse()
+              .map((history, index) => (
+                <HistoryText key={`history.${index}`}>{history}</HistoryText>
+              ))}{" "}
+            <Footer>
+              <CloseButton
+                onClick={(): void => {
+                  setTimeout(() => {
+                    dispatch({ type: "SET_SHOW_HISTORY", showHistory: false });
+                  }, 300);
+                }}
+              />
+            </Footer>
+          </List>
+        </Wrapper>
+      ) : null}
+    </>
   );
 }
