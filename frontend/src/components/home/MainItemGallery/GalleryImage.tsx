a74d75dbee88111c58864548d2bbc8df1efe6064
brightchul <abhbtbb@gmail.com>
2020-08-27 02:10:55 +0900

    feat: 메인 이미지 갤러리 url 적용
    - s3 url 적용 되도록 변경했다.

--- frontend/src/components/home/MainItemGallery/GalleryImage.tsx
+++ frontend/src/components/home/MainItemGallery/GalleryImage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { imgURL } from "../../../utils/func";
 
 type Props = {
   src?: string;
@@ -21,7 +22,7 @@ export default function GalleryImage({
   onClick,
   index,
 }: Props): JSX.Element {
-  const style = { backgroundImage: `url("${src}")` };
+  const style = { backgroundImage: `url("${imgURL(src)}")` };
 
   return (
     <ImgDiv
